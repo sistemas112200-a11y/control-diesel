e9b57150a871059e47781d25cca67f2bd1a18f7c
sistemas112200-a11y <sistemas.112200@gmail.com>
2026-07-22 11:49:30 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  outputFileTracingIncludes: {
+    "/reportes-unidad/[id]/pdf": ["./node_modules/pdfkit/js/data/**/*"],
+  },
 };
 
 export default nextConfig;
